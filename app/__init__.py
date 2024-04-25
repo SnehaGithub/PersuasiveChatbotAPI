adfbc8df3a470e647a49f10d2f91c800f7a53d62
SnehaGithub <snehaiyer01@gmail.com>
2024-04-25 14:45:32 -0400

    fix cors

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 # Configure Flask app
 app.config['SECRET_KEY'] = os.environ.get("FLASK_SECRET_KEY", "default_secret")
-CORS(app, resources={r"/compare_message": {"origins": "*"}})
+CORS(app, resources={r"/compare_message": {"origins": "https://persuasivechatbotapp.onrender.com"}})
 
 # Define routes here, avoiding circular imports
 @app.route('/compare_message', methods=['POST'])
